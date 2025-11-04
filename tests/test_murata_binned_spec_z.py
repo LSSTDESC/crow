2682add3bee966d111aa15418603942f42c382d3
Michel <aguena@lapp.in2p3.fr>
2025-11-04 18:53:30 +0100

    fix typo

--- tests/test_murata_binned_spec_z.py
+++ tests/test_murata_binned_spec_z.py
@@ -15,7 +15,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from crow.integrator.numcosmo_integrator import NumCosmoIntegrator
 from crow.kernel import SpectroscopicRedshift
 from crow.mass_proxy import MurataBinned
-from crow.property import ClusterProperty
+from crow.properties import ClusterProperty
 from crow.recipes.murata_binned_spec_z import MurataBinnedSpecZRecipe
 
 # from firecrown.models.cluster import ClusterProperty
