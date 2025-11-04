e697cbc23de333d0a90df5635e1ec21deaa433f6
Michel <aguena@lapp.in2p3.fr>
2025-11-04 17:26:16 +0100

    run black

--- crow/recipes/murata_binned_spec_z.py
+++ crow/recipes/murata_binned_spec_z.py
@@ -13,8 +13,9 @@ from crow.kernel import SpectroscopicRedshift
 from crow.mass_proxy import MurataBinned
 
 from crow.properties import ClusterProperty
+
 # To run with firecrown, use this import instead
-#from firecrown.models.cluster import ClusterProperty
+# from firecrown.models.cluster import ClusterProperty
 
 
 class MurataBinnedSpecZRecipe:
