ce36c3e569b0c725d5a7e6b777d3616e6f555c27
Michel <aguena@lapp.in2p3.fr>
2025-11-04 15:42:22 +0100

    update recipe

--- tests/test_murata_binned_spec_z_delta_sigma.py
+++ tests/test_murata_binned_spec_z_delta_sigma.py
@@ -9,7 +9,7 @@ import pytest
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
+from crow.deltasigma import ClusterShearProfile
 from crow.integrator.numcosmo_integrator import NumCosmoIntegrator
 from crow.kernel import SpectroscopicRedshift
 from crow.mass_proxy import MurataBinned
@@ -41,8 +41,16 @@ def fixture_murata_binned_spec_z() -> MurataBinnedSpecZRecipe:
 @pytest.fixture(name="murata_binned_spec_z_deltasigma")
 def fixture_murata_binned_spec_z_deltasigma() -> MurataBinnedSpecZDeltaSigmaRecipe:
     pivot_mass, pivot_redshift = 14.625862906, 0.6
+    cluster_theory = ClusterShearProfile(
+        z_interval=(0, 2),
+        mass_interval=(13, 17),
+        halo_mass_function=pyccl.halos.MassFuncTinker08(mass_def="200c"),
+        cluster_concentration=4.0,
+        is_delta_sigma=True,
+    )
+    cluster_theory.set_beta_parameters(10.0)
     cluster_recipe = MurataBinnedSpecZDeltaSigmaRecipe(
-        hmf=pyccl.halos.MassFuncTinker08(mass_def="200c"),
+        cluster_theory=cluster_theory,
         redshift_distribution=SpectroscopicRedshift(),
         mass_distribution=MurataBinned(pivot_mass, pivot_redshift),
     )
@@ -59,8 +67,16 @@ def fixture_murata_binned_spec_z_deltasigma() -> MurataBinnedSpecZDeltaSigmaReci
 
 def test_murata_binned_spec_z_deltasigma_init():
     pivot_mass, pivot_redshift = 14.625862906, 0.6
+    cluster_theory = ClusterShearProfile(
+        z_interval=(0, 2),
+        mass_interval=(13, 17),
+        halo_mass_function=pyccl.halos.MassFuncTinker08(mass_def="200c"),
+        cluster_concentration=4.0,
+        is_delta_sigma=True,
+    )
+    cluster_theory.set_beta_parameters(10.0)
     recipe = MurataBinnedSpecZDeltaSigmaRecipe(
-        hmf=pyccl.halos.MassFuncTinker08(mass_def="200c"),
+        cluster_theory=cluster_theory,
         redshift_distribution=SpectroscopicRedshift(),
         mass_distribution=MurataBinned(pivot_mass, pivot_redshift),
     )
