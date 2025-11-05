c055b5a8e938012eab66b6023ba5c7ecfbdf0b1a
Michel <aguena@lapp.in2p3.fr>
2025-11-05 14:55:03 +0100

    remove leftover

--- crow/__init__.py
+++ crow/__init__.py
@@ -3,5 +3,3 @@
 from .cluster_modules import kernel, mass_proxy
 from .cluster_modules.abundance import ClusterAbundance
 from .cluster_modules.shear_profile import ClusterShearProfile
-
-mass_proxy
