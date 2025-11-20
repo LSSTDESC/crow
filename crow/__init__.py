1df907e0f4241a01569e1ce26e30a98f557de0d9
Michel <aguena@lapp.in2p3.fr>
2025-11-20 12:01:18 +0100

    test with bad version

--- crow/__init__.py
+++ crow/__init__.py
@@ -4,4 +4,4 @@ from .cluster_modules import completeness, kernel, mass_proxy, purity
 from .cluster_modules.abundance import ClusterAbundance
 from .cluster_modules.shear_profile import ClusterShearProfile
 
-__version__ = "0.0.2"
+__version__ = "0.0.0"
