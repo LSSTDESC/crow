5c0efa49cb0dd3e46cd62b39ae4256b66e172989
Michel Aguena <aguena@inaf.it>
2025-11-25 20:19:13 +0100

    vectorize 2h term for shear too

--- crow/cluster_modules/shear_profile.py
+++ crow/cluster_modules/shear_profile.py
@@ -26,68 +26,6 @@ from .completeness import Completeness
 from .parameters import Parameters
 
 
-def numcosmo_miscentered_mean_surface_density(  # pragma: no cover
-    r_proj, r_mis, integrand, norm, aux_args, extra_integral
-):
-    """
-    NumCosmo replacement for `integrate_azimuthially_miscentered_mean_surface_density`.
-
-    Integrates azimuthally and radially for the mean surface mass density kernel.
-    """
-    integrator = NumCosmoIntegrator(
-        relative_tolerance=1e-6,
-        absolute_tolerance=1e-3,
-    )
-    integrand = np.vectorize(integrand)
-    r_proj = np.atleast_1d(r_proj)
-    r_lower = np.full_like(r_proj, 1e-6)
-    r_lower[1:] = r_proj[:-1]
-
-    results = []
-    args = (r_mis, *aux_args)
-    integrator.extra_args = np.array(args)
-    for r_low, r_high in zip(r_lower, r_proj):
-        if extra_integral:
-            integrator.integral_bounds = [
-                (r_low, r_high),
-                (1.0e-6, np.pi),
-                (0.0, np.inf),
-            ]
-
-            def integrand_numcosmo(int_args, extra_args):
-                r_local = int_args[:, 0]
-                theta = int_args[:, 1]
-                extra = int_args[:, 2]
-                return integrand(theta, r_local, extra, *extra_args)
-
-        else:
-            integrator.integral_bounds = [(r_low, r_high), (1.0e-6, np.pi)]
-
-            def integrand_numcosmo(int_args, extra_args):
-                r_local = int_args[:, 0]
-                theta = int_args[:, 1]
-                return integrand(theta, r_local, *extra_args)
-
-        res = integrator.integrate(integrand_numcosmo)
-        results.append(res)
-
-    results = np.array(results)
-    mean_surface_density = np.cumsum(results) * norm * 2 / np.pi / r_proj**2
-    if not np.iterable(r_proj):
-        return res[0] * norm * 2 / np.pi / r_proj**2
-    return mean_surface_density
-
-
-clmm.theory.miscentering.integrate_azimuthially_miscentered_mean_surface_density = (  # pragma: no cover
-    numcosmo_miscentered_mean_surface_density
-)
-
-# To circumvent a bug in CLMM
-clmm.cosmology.ccl.CLMMCosmology.get_a_from_z = (  # pragma: no cover
-    clmm.cosmology.ccl.CLMMCosmology._get_a_from_z  # pragma: no cover
-)
-
-
 class ClusterShearProfile(ClusterAbundance):
     """The class that calculates the predicted delta sigma of galaxy clusters.
 
@@ -270,7 +208,12 @@ class ClusterShearProfile(ClusterAbundance):
         if self.vectorized:
             moo._set_concentration(self._get_concentration(log_mass, z))
             moo._set_mass(10**log_mass)
-            return self._one_halo_contribution(moo, radius_center, z)
+            return_vals = self._one_halo_contribution(moo, radius_center, z)
+            if self.two_halo_term:
+                return_vals += moo._eval_excess_surface_density_2h(radius_center, z)
+            if self.boost_factor:
+                return_vals = self._correct_with_boost_nfw(return_vals, radius_center)
+            return return_vals
 
         return_vals = []
         for log_m, redshift in zip(log_mass, z):
@@ -333,8 +276,8 @@ class ClusterShearProfile(ClusterAbundance):
         if self.is_delta_sigma == False:
             raise Exception("Two halo contribution for gt is not suported yet.")
 
-        second_halo_right_centered = clmm_model.eval_excess_surface_density_2h(
-            np.array([radius_center]), redshift
+        second_halo_right_centered = clmm_model._eval_excess_surface_density_2h(
+            np.atleast_1d(radius_center), np.atleast_1d(redshift)
         )
 
         return second_halo_right_centered[0]
@@ -453,5 +396,164 @@ class ClusterShearProfile(ClusterAbundance):
         integrator.integral_bounds = [(0.0, integration_max)]
         integrator.extra_args = np.array([sigma])
         miscentering_integral = integrator.integrate(integration_func)
-
         return miscentering_integral, miscentering_frac
+
+
+##############################
+# Monkeypatch CLMM functions #
+##############################
+
+
+def numcosmo_miscentered_mean_surface_density(  # pragma: no cover
+    r_proj, r_mis, integrand, norm, aux_args, extra_integral
+):
+    """
+    NumCosmo replacement for `integrate_azimuthially_miscentered_mean_surface_density`.
+
+    Integrates azimuthally and radially for the mean surface mass density kernel.
+    """
+    integrator = NumCosmoIntegrator(
+        relative_tolerance=1e-6,
+        absolute_tolerance=1e-3,
+    )
+    integrand = np.vectorize(integrand)
+    r_proj = np.atleast_1d(r_proj)
+    r_lower = np.full_like(r_proj, 1e-6)
+    r_lower[1:] = r_proj[:-1]
+
+    results = []
+    args = (r_mis, *aux_args)
+    integrator.extra_args = np.array(args)
+    for r_low, r_high in zip(r_lower, r_proj):
+        if extra_integral:
+            integrator.integral_bounds = [
+                (r_low, r_high),
+                (1.0e-6, np.pi),
+                (0.0, np.inf),
+            ]
+
+            def integrand_numcosmo(int_args, extra_args):
+                r_local = int_args[:, 0]
+                theta = int_args[:, 1]
+                extra = int_args[:, 2]
+                return integrand(theta, r_local, extra, *extra_args)
+
+        else:
+            integrator.integral_bounds = [(r_low, r_high), (1.0e-6, np.pi)]
+
+            def integrand_numcosmo(int_args, extra_args):
+                r_local = int_args[:, 0]
+                theta = int_args[:, 1]
+                return integrand(theta, r_local, *extra_args)
+
+        res = integrator.integrate(integrand_numcosmo)
+        results.append(res)
+
+    results = np.array(results)
+    mean_surface_density = np.cumsum(results) * norm * 2 / np.pi / r_proj**2
+    if not np.iterable(r_proj):
+        return res[0] * norm * 2 / np.pi / r_proj**2
+    return mean_surface_density
+
+
+from scipy.interpolate import splev, splrep
+from scipy.special import gamma, gammainc, jv
+from scipy.integrate import quad, simpson
+
+
+def _eval_2halo_term_generic(
+    ####################################################################################
+    # NOTE: This function is just a small optimization of the one implemented on CLMM  #
+    # here just to benchmark the difference due the restructuration of the integration #
+    ####################################################################################
+    self,
+    sph_harm_ord,
+    r_proj,
+    z_cl,
+    halobias=1.0,
+    logkbounds=(-5, 5),
+    ksteps=1000,
+    loglbounds=(0, 6),
+    lsteps=500,
+):
+    """eval excess surface density from the 2-halo term"""
+    # pylint: disable=protected-access
+    da = self.cosmo.eval_da(z_cl)
+    rho_m = self.cosmo._get_rho_m(z_cl)
+    theta = r_proj / da
+
+    # interp pk
+    _k_values = np.logspace(logkbounds[0], logkbounds[1], ksteps)
+    interp_pk = splrep(
+        _k_values, self.cosmo._eval_linear_matter_powerspectrum(_k_values, z_cl)
+    )
+
+    # integrate
+    l_values = np.logspace(loglbounds[0], loglbounds[1], lsteps)
+    kernel = simpson(
+        (
+            l_values
+            * jv(sph_harm_ord, l_values * theta)
+            * splev(l_values / ((1 + z_cl) * da), interp_pk)
+        ),
+        x=l_values,
+    )
+    return [halobias * kernel * rho_m / (2 * np.pi * (1 + z_cl) ** 3 * da**2)]
+
+
+def _eval_2halo_term_generic_vec(
+    self,
+    sph_harm_ord,
+    r_proj,
+    z_cl,
+    halobias=1.0,
+    logkbounds=(-5, 5),
+    ksteps=1000,
+    loglbounds=(0, 6),
+    lsteps=500,
+):
+    """eval excess surface density from the 2-halo term"""
+    # pylint: disable=protected-access
+    da = self.cosmo.eval_da(z_cl)
+    rho_m = self.cosmo._get_rho_m(z_cl)
+
+    # (n_z, n_r)
+    theta = (r_proj / da[None, :]).T
+
+    # calculate integral, units [Mpc]**-3
+    l_values = np.logspace(loglbounds[0], loglbounds[1], lsteps)
+    # print("l_values:", l_values.shape)
+
+    # (n_l, n_z)
+    k_values = l_values[:, np.newaxis] / ((1 + z_cl) * da)
+    pk_values = np.zeros(k_values.shape)
+    for i in range(z_cl.size):
+        pk_values[:, i] = self.cosmo._eval_linear_matter_powerspectrum(
+            k_values[:, i], z_cl[i]
+        )
+
+    # (n_l, n_z, n_r)
+    jv_values = jv(sph_harm_ord, l_values[:, None, None] * theta[None, :, :])
+    kernel = l_values[:, None, None] * pk_values[:, :, None] * jv_values
+
+    # (n_z, n_r)
+    integ = simpson(kernel, x=l_values, axis=0)
+
+    # (n_z, n_r)
+    out = halobias * integ * (rho_m / (2 * np.pi * (1 + z_cl) ** 3 * da**2))[:, None]
+
+    # (n_r, n_z)
+    return out.T
+
+
+clmm.theory.miscentering.integrate_azimuthially_miscentered_mean_surface_density = (  # pragma: no cover
+    numcosmo_miscentered_mean_surface_density
+)
+clmm.Modeling._eval_2halo_term_generic = (  # pragma: no cover
+    _eval_2halo_term_generic_vec
+)
+
+# To circumvent a bug in CLMM
+clmm.cosmology.ccl.CLMMCosmology.get_a_from_z = (  # pragma: no cover
+    clmm.cosmology.ccl.CLMMCosmology._get_a_from_z
+)
