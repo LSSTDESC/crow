6c03598d635c4ffbc74de160718744e60cf98fdb
Michel Aguena <aguena@inaf.it>
2025-11-27 17:35:17 +0100

    remove purity from mass_dist

--- crow/recipes/binned_parent.py
+++ crow/recipes/binned_parent.py
@@ -4,6 +4,7 @@ import numpy as np
 import numpy.typing as npt
 
 from crow import completeness as comp
+from crow import purity as pur
 from crow.properties import ClusterProperty
 
 # To run with firecrown, use this import instead
@@ -23,31 +24,28 @@ class BinnedClusterRecipe:
 
     @completeness.setter
     def completeness(self, completeness: comp.Completeness) -> None:
-        """Update the cluster abundance calculation with a new completeness."""
+        """Update the cluster recipe with a new completeness."""
         self.__completeness = completeness
-        if completeness is None:
-            self._completeness_distribution = self._complete_distribution
-        else:
-            self._completeness_distribution = self._incomplete_distribution
+        self._setup_with_completeness()
 
     @property
-    def purity(self) -> comp.Completeness | None:
-        """The completeness used to predict the cluster number count."""
-        return self.mass_distribution.purity
+    def purity(self) -> pur.Purity | None:
+        """The purity used to predict the cluster number count."""
+        return self.__purity
 
-    def _complete_distribution(
+    @purity.setter
+    def purity(self, purity: pur.Purity) -> None:
+        """Update the cluster recipe calculation with a new purity."""
+        self.__purity = purity
+        self._setup_with_purity()
+
+    def _flat_distribution(
         self,
         log_mass: npt.NDArray[np.float64],
         z: npt.NDArray[np.float64],
     ):
-        return 1.0
-
-    def _incomplete_distribution(
-        self,
-        log_mass: npt.NDArray[np.float64],
-        z: npt.NDArray[np.float64],
-    ):
-        return self.completeness.distribution(log_mass, z)
+        """Returns a null (=1) contribution to the integrand."""
+        return 1.0 + 0 * log_mass * z
 
     def __init__(
         self,
@@ -55,6 +53,7 @@ class BinnedClusterRecipe:
         redshift_distribution,
         mass_distribution,
         completeness: comp.Completeness = None,
+        purity: pur.Purity = None,
         mass_interval: tuple[float, float] = (11.0, 17.0),
         true_z_interval: tuple[float, float] = (0.0, 5.0),
     ) -> None:
@@ -63,17 +62,23 @@ class BinnedClusterRecipe:
         self.redshift_distribution = redshift_distribution
         self.mass_distribution = mass_distribution
         self.completeness = completeness
+        self.purity = purity
         self.mass_interval = mass_interval
         self.true_z_interval = true_z_interval
 
-    def completeness_distribution(
-        self,
-        log_mass: npt.NDArray[np.float64],
-        z: npt.NDArray[np.float64],
-    ) -> npt.NDArray[np.float64]:
-        """Evaluates and returns the completeness contribution to the integrand."""
+    def _setup_with_completeness(self):
+        """Additional setup of class with the completeness"""
+        if self.completeness is None:
+            self.completeness_distribution = self._flat_distribution
+        else:
+            self.completeness_distribution = self.completeness.distribution
 
-        return self._completeness_distribution(log_mass, z)
+    def _setup_with_purity(self):
+        """Additional setup of class with the purity"""
+        if self.purity is None:
+            self.purity_distribution = self._flat_distribution
+        else:
+            self.purity_distribution = self.purity.distribution
 
     ##############################################
     # Functions to be implemented in child classes
