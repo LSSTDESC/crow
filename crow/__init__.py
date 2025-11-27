4ca190df2b497f60a70fbc68fdb20ffdd87c0768
Caio Lima de Oliveira <caiolimadeoliveira@pm.me>
2025-11-28 00:22:17 +0100

    Remove automatic versioning and bump version

--- crow/__init__.py
+++ crow/__init__.py
@@ -4,7 +4,4 @@ from .cluster_modules import completeness, kernel, mass_proxy, purity
 from .cluster_modules.abundance import ClusterAbundance
 from .cluster_modules.shear_profile import ClusterShearProfile
 
-try:
-    from ._version import version as __version__
-except ImportError:
-    __version__ = "0.0.0+unknown"
+__version__ = "0.1.1"
