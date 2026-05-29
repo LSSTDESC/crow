67ae84764e7c2a6c9bbb40cd9ce25afc92071362
I-Non Chiu <inchiusaiyan@gmail.com>
2026-05-29 22:54:16 +0800

    create a new dir. copy costanzi.py here. slightly modify the code.

---
+++ crow/cluster_modules/projection_effects/__init__.py
@@ -0,0 +1,8 @@
+"""The projection effect module.
+
+This module holds the classes that define the projection effects
+that can be included in the richness-mass relation.
+"""
+
+#from .murata import MurataBinned, MurataUnbinned
+#from .costanzi import CostanziBaseModel, CostanziBinned
